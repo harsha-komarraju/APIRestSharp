eb47828612828da11cac67fe9678f8b41bf5bb43
Harsha <harsha.komarraju@hotmail.com>
2022-10-30 20:25:10 +0000

    Add project files.

---
+++ APIRestSharp/APITest.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System;
+using System.Text.Json;
+using Assert = NUnit.Framework.Assert;
+
+namespace APIRestSharp
+{
+    [TestClass]
+    public class EnableTest
+    {
+        [TestMethod]
+        public void GetRequestValidation()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts/{postid}", Method.Get);
+            request.AddUrlSegment("postid", 3);
+
+            var response = client.Execute(request);
+            var output = JsonSerializer.Deserialize<Rootobject>(response.Content);
+            try
+            {
+                Assert.IsTrue(output.author.Equals("Harsha") && output.id.Equals(3) && output.title.Equals("enable test"));
+                Console.WriteLine("All responses are correct");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Output response is different - FAIL");
+            }
+        }
+
+        [TestMethod]
+        public void GetRequestFailTest()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts/{postid}", Method.Get);
+            request.AddUrlSegment("postid", 3);
+
+            var response = client.Execute(request);
+            var output = JsonSerializer.Deserialize<Rootobject>(response.Content);
+            try
+            {
+                Assert.IsTrue(output.author.Equals("Harsha") && output.id.Equals(1) && output.title.Equals("enable test"));
+                Console.WriteLine("All responses are correct");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Output response is different - FAIL");
+            }
+        }
+
+        [TestMethod]
+        public void PostRequestValidation()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var newRequest = new RestRequest("posts", Method.Post);
+            newRequest.RequestFormat = DataFormat.Json;
+            newRequest.AddBody(new { id = 6, title = "Enable API3", author = "Harsha k3" });
+            var response = client.Execute(newRequest);
+            var output = JsonSerializer.Deserialize<Rootobject>(response.Content);
+            Assert.IsTrue(output.author.Equals("Harsha k3") && output.id.Equals(6) && output.title.Equals("Enable API3"));
+              
+           
+        }
+    }
+}
---
+++ APIRestSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("APIRestSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("APIRestSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c36552b7-3809-4e15-ab36-9841fc102e17")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
